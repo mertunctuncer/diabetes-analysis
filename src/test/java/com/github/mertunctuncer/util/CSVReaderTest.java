9cf0766b5d6910603b71c7cfd821eb497e008db4
mertunctuncer <mertunctuncer@gmail.com>
2024-12-19 12:10:14 +0300

    Implement test

--- src/test/java/com/github/mertunctuncer/util/CSVReaderTest.java
+++ src/test/java/com/github/mertunctuncer/util/CSVReaderTest.java
@@ -1,10 +1,22 @@
 package com.github.mertunctuncer.util;
 
+import com.github.mertunctuncer.Main;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
 
 class CSVReaderTest {
 
     @Test
     void fileReadCorrectly() {
+
+        Assertions.assertDoesNotThrow(() -> {
+            CSVReader reader = new CSVReader(Paths.get(Main.class.getClassLoader().getResource("diabetes_012.csv").toURI()));
+            reader.readFile();
+            Assertions.assertEquals(0, reader.getData()[0][0]);
+            Assertions.assertEquals(40.0, reader.getData()[0][4]);
+            Assertions.assertEquals(22, reader.getColumnCount());
+        });
     }
 }
